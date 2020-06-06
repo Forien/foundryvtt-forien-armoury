e6426e00a91e12906020a03381b5be924cb46a26
Forien <mr.forien@gmail.com>
2020-06-06 20:56:05 +0200

    fixed arrow reclamation working for GM only by adding sockets (#3)
    * init branch
    
    * arrow feature
    
    * changed script to match system's updated hook
    
    * wtf git
    
    * .
    
    * fixed arrow reclamation working for GM only by adding sockets

--- scripts/arrows-homerule.js
+++ scripts/arrows-homerule.js
@@ -1,17 +1,13 @@
 class ForienArmoury {
 	/**
+	 * Adds information about Ammunition entity and Actor entity to Combat Tracker
+	 * allows for tracking which ammo and in what quantity has to be returned
 	 */
 	static addAmmoToReplenish(actorId, ammoId) 
 	{
-		// retrieve array or initialize it. 
-		let ammoReplenish = game.combat.ammoReplenish || [];
-		game.combat.ammoReplenish = ammoReplenish;
-		
-		// retrieve actor array or initialize it.
+		// retrieve existing data or initialize it
+		let ammoReplenish = game.combat.getFlag('forien-armoury', 'ammoReplenish') || {};
 		let actorData = ammoReplenish[actorId] || [];
-		ammoReplenish[actorId] = actorData;
-	
-		// retrieve ammo object
 		let ammoData = actorData.find(a => a._id == ammoId);
 
 		// if ammo object doesn't exist, create one
@@ -20,15 +16,19 @@ class ForienArmoury {
 				"_id": ammoId,
 				"quantity": 0
 			};
-			// push ammo object to actor data
 			actorData.push(ammoData);
 		}
-		
-		// increase ammo to replenish
 		ammoData.quantity += 1;
+		
+		// overwrite actor data
+		ammoReplenish[actorId] = actorData;
+		
+		// set (overwrite) flag with updated data
+		game.combat.setFlag('forien-armoury', 'ammoReplenish', ammoReplenish);
 	}
 	
 	/**
+	 * Finds ammo in possesion of an Actor and replenishes given amount
 	 */
 	static replenishAmmo(actorId, ammoId, quantity) 
 	{
@@ -40,26 +40,34 @@ class ForienArmoury {
 	}
 	
 	/**
+	 * When combat ends, check Combat Tracker for any ammunition to recover.
 	 */
-	static processEndOfCombat() 
+	static processEndOfCombat(combat) 
 	{
-		if (Array.isArray(game.combat.ammoReplenish)) {
-			for (var actorId in game.combat.ammoReplenish) {
-				if (Array.isArray(game.combat.ammoReplenish[actorId])) {					
-					game.combat.ammoReplenish[actorId].forEach(function (ammo) {
-						ForienArmoury.replenishAmmo(actorId, ammo._id, ammo.quantity);
-					});
-				}
+		let ammoReplenish = combat.getFlag('forien-armoury', 'ammoReplenish');
+
+		for (var actorId in ammoReplenish) {
+			if (Array.isArray(ammoReplenish[actorId])) {
+				ammoReplenish[actorId].forEach(function (ammo) {
+					ForienArmoury.replenishAmmo(actorId, ammo._id, ammo.quantity);
+				});
 			}
 		}
 	}
 }
 
-Hooks.on("preDeleteCombat", function() {
-	ForienArmoury.processEndOfCombat();
+Hooks.on("deleteCombat", function(combat) {
+	if (game.user.isGM) {
+		ForienArmoury.processEndOfCombat(combat);
+	}
 })
 
 Hooks.on("wfrp4e:rollWeaponTest", function(roll, cardOptions) {
+	// if feature not enabled, do nothing
+	if (!game.settings.get("forien-armoury", "arrowReclamationEnable")) {
+		return;
+	}
+
 	let actorId = cardOptions.speaker.actor;
 	let weapon = roll.weapon;
 	let ammoId = weapon.data.currentAmmo.value;
@@ -70,32 +78,30 @@ Hooks.on("wfrp4e:rollWeaponTest", function(roll, cardOptions) {
 	let save = false;
 	let message = "";
 	
-	if (!game.settings.get("forien-armoury", "arrowReclamationEnable")) {
-		return;
-	}
-	
+	// prepare ammo type for translation and also limit types of ammo being recoverable
 	if (weapon.data.ammunitionGroup.value == 'bow') {
 		type = 'Arrow';
 	} else if (weapon.data.ammunitionGroup.value == 'crossbow') {
 		type = 'Bolt';
+	} else if (weapon.data.ammunitionGroup.value == 'sling') {
+		type = 'Bullet';
 	}
 	
 	if (type == null) {
 		return;
 	}
 	
-	let messageNow = `${game.i18n.localize(type)} ${game.i18n.localize("recovered")}.`;
-	let messageFuture = `${game.i18n.localize(type)} ${game.i18n.localize("will be recovered after combat")}.`;
+	// define chat messages
+	let messageNow = `${game.i18n.localize("FArmoury." + type)} ${game.i18n.localize("FArmoury.recovered")}.`;
+	let messageFuture = `${game.i18n.localize("FArmoury." + type)} ${game.i18n.localize("FArmoury.recoveredFuture")}.`;
 
-	
+	// Define flags used by rules
 	if (roll.extra.critical != undefined || roll.extra.fumble != undefined) {
 		crit = true;
 	}
-	
 	if (roll.roll % 2 == 0) {
 		even = true;
 	}
-	
 	if (roll.roll <= roll.target) {
 		success = true;
 	}
@@ -122,7 +128,14 @@ Hooks.on("wfrp4e:rollWeaponTest", function(roll, cardOptions) {
 			ForienArmoury.replenishAmmo(actorId, ammoId, 1);
 		} else {
 			message = messageFuture;
-			ForienArmoury.addAmmoToReplenish(actorId, ammoId);
+			if (game.user.isGM) {
+				ForienArmoury.addAmmoToReplenish(actorId, ammoId);
+			} else {				
+				game.socket.emit("module.forien-armoury", {
+					type: "arrowToReclaim",
+					payload:  {actorId:actorId, ammoId:ammoId}
+				})
+			}
 		}
 		
 		if (Array.isArray(roll.other)) {
@@ -134,6 +147,16 @@ Hooks.on("wfrp4e:rollWeaponTest", function(roll, cardOptions) {
 });
 
 Hooks.on("init", function() {
+	// Allow and process incoming socket data
+	game.socket.on("module.forien-armoury", data => {
+		if (game.user.isGM) {
+			if (data.type == "arrowToReclaim") {
+				ForienArmoury.addAmmoToReplenish(data.payload.actorId, data.payload.ammoId);
+			}
+		}
+	})	
+	
+	// Add enable/disable setting for arrow reclamation feature
 	game.settings.register("forien-armoury", "arrowReclamationEnable", {
 		name: "SETTINGS.arrowReclamationEnable",
 		hint: "SETTINGS.arrowReclamationEnableHint",
@@ -143,6 +166,7 @@ Hooks.on("init", function() {
 		type: Boolean
 	});
 	
+	// Add setting that allows for different rules of arrow reclamation
 	game.settings.register("forien-armoury", "arrowReclamationRule", {
 		name: "SETTINGS.arrowReclamationRule",
 		hint: "SETTINGS.arrowReclamationRuleHint",
