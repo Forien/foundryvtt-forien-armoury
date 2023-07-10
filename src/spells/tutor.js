82998e4df3dde66b77fa70fa2ca03b283280d95b
Forien <mr.forien@gmail.com>
2023-07-10 17:55:46 +0200

    prepared runebound spells

---
+++ src/spells/tutor.js
@@ -0,0 +1,12 @@
+let attackerSize = args.attacker.sizeNum;
+let mySize = args.actor.sizeNum
+
+if (attackerSize > mySize) {
+  let sizeDiff = (attackerSize - mySize) * 2;
+  args.AP.value += sizeDiff;
+
+  let name = game.i18n.localize('Forien.Armoury.Runebound.Tutor.Name');
+  let effect = game.i18n.format('Forien.Armoury.Runebound.Tutor.Effect', {num: sizeDiff});
+  args.extraMessages.push(`<strong>${name}</strong> ${effect}`);
+}
+
